5b2b646416a79b7925fb06eb86914ff91c16c9e8
AndersonIvanildo <andersonivanildo@protonmail.com>
2026-07-07 17:18:27 -0300

    test blink led

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,14 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  Serial.println("Projeto Iniciado!");
+  pinMode(LED_BUILTIN, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
